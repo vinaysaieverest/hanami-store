9ccfb6321ecec6aaedb5f03b3859f29ad2dd606c
Admin <admin@Admins-MacBook-Pro-8.local>
2024-08-26 02:37:17 +0530

    Implimented the back end using MongoDB

--- src/Context/GlobalContext.tsx
+++ src/Context/GlobalContext.tsx
@@ -1,11 +1,7 @@
-import React, { Children, createContext, useEffect, useState } from "react";
-import { data } from "../data";
-import { Ttypecard } from "../types";
-
+import React, {  createContext, useEffect, useState } from "react";
 export const dataContext = createContext<any>(null);
-
 export default function GlobalContext(props: any) {
-  const [Data, setData] = useState(data);
+  const [Data, setData] = useState([]);
   const [noOfCartItem, setNoOfCartItem] = useState(0);
   const [noOfItems, setnoOfItems] = useState(1);
   const [isAdded, setisAdded] = useState(false);
@@ -13,18 +9,27 @@ export default function GlobalContext(props: any) {
   const [search, setSearch] = useState("");
   const [isLiked, setisLiked] = useState(false);
   const [size, setSize] = useState("M");
+  
+  
 
-  useEffect(()=>{
-    fetch("http://localhost:4000/api/items")
+  useEffect(()=> {
+    fetch("http://localhost:5001/api/items/")
       .then((response) => response.json())
-      .then((data: Ttypecard[]) => {
-        console.log(data);
-        const Results = data.filter((item) =>
-          item.name.toLowerCase().includes(search.toLowerCase())
-        );
-        setData(Results);
-      });
-  },[search]);
+      .then((result)=>{setData(result)});
+
+  },[])
+
+  // useEffect(()=>{
+  //   fetch("http://localhost:5001/api/items/")
+  //     .then((response) => response.json())
+  //     .then((data: Ttypecard[]) => {
+  //       console.log(data);
+  //       const Results:any = data.filter((item) =>
+  //         item.name.toLowerCase().includes(search.toLowerCase())
+  //       );
+  //       setData(Results);
+  //     });
+  // },[search]);
 
 
   const value = {
